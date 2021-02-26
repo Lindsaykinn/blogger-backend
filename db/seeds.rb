52d271e4909c48dc51cba004c30fbb2b05b0bb1c
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-26 17:08:42 -0500

    initial commit, added Rails API

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+author_1 = Author.create(name: "Bob")
+author_2 = Author.create(name: "Sarah")
+  
+Blog.create([
+  { title: "Blog Title", content: "This is a blog.", author: author_1 },
+  { title: "Blog Title 2", content: "This is a blog 2.", author: author_2},
+  { title: "Blog Title 3", content: "This is a blog 3.", author: author_1},
+  { title: "Blog Title 4", content: "This is a blog 4.", author: author_2}
+])
